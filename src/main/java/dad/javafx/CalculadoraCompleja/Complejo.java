ab93cb5ba39f9f0af0c29195c49ba94f14f7ef0c
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-10-21 11:48:49 +0000

    Update Complejo.java

--- src/main/java/dad/javafx/CalculadoraCompleja/Complejo.java
+++ src/main/java/dad/javafx/CalculadoraCompleja/Complejo.java
@@ -1,7 +1,44 @@
 package dad.javafx.CalculadoraCompleja;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
 public class Complejo {
 
-	
-	
+	private DoubleProperty real = new SimpleDoubleProperty();
+	private DoubleProperty imaginario = new SimpleDoubleProperty();
+
+	public Complejo() {
+	}
+
+	public Complejo(double real, double imaginario) {
+		super();
+		setReal(real);
+		setImaginario(imaginario);
+	}
+
+	public final DoubleProperty realProperty() {
+		return this.real;
+	}
+
+	public final double getReal() {
+		return this.realProperty().get();
+	}
+
+	public final void setReal(final double real) {
+		this.realProperty().set(real);
+	}
+
+	public final DoubleProperty imaginarioProperty() {
+		return this.imaginario;
+	}
+
+	public final double getImaginario() {
+		return this.imaginarioProperty().get();
+	}
+
+	public final void setImaginario(final double imaginario) {
+		this.imaginarioProperty().set(imaginario);
+	}
+
 }
