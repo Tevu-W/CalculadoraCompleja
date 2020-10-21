961d9cc1cdf2eebaebb6c549e28d79dedafe88ae
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-10-21 11:48:53 +0000

    Update CalculadoraCompleja.java

--- src/main/java/dad/javafx/CalculadoraCompleja/CalculadoraCompleja.java
+++ src/main/java/dad/javafx/CalculadoraCompleja/CalculadoraCompleja.java
@@ -1,6 +1,7 @@
 package dad.javafx.CalculadoraCompleja;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -15,7 +16,6 @@ import javafx.stage.Stage;
 public class CalculadoraCompleja extends Application {
 
 	private TextField real1, imagin1, real2, imagin2, resul1, resul2;
-	private Label i, suma;
 	private ComboBox<String> ops;
 	private Separator separator;
 
@@ -24,32 +24,34 @@ public class CalculadoraCompleja extends Application {
 		// TODO Auto-generated method stub
 
 		real1 = new TextField();
-		real1.setPrefColumnCount(5);
+		real1.setPrefColumnCount(3);
 		real1.setText("0");
+		real1.setAlignment(Pos.CENTER);
 
 		imagin1 = new TextField();
-		imagin1.setPrefColumnCount(5);
+		imagin1.setPrefColumnCount(3);
 		imagin1.setText("0");
+		imagin1.setAlignment(Pos.CENTER);
 
 		real2 = new TextField();
-		real2.setPrefColumnCount(5);
+		real2.setPrefColumnCount(3);
 		real2.setText("0");
+		real2.setAlignment(Pos.CENTER);
 
 		imagin2 = new TextField();
-		imagin2.setPrefColumnCount(5);
+		imagin2.setPrefColumnCount(3);
 		imagin2.setText("0");
+		imagin2.setAlignment(Pos.CENTER);
 
 		resul1 = new TextField();
-		resul1.setPrefColumnCount(5);
+		resul1.setPrefColumnCount(3);
 		resul1.setText("0");
+		resul1.setAlignment(Pos.CENTER);
 
 		resul2 = new TextField();
-		resul2.setPrefColumnCount(5);
+		resul2.setPrefColumnCount(3);
 		resul2.setText("0");
-
-		i = new Label("i");
-
-		suma = new Label("+");
+		resul2.setAlignment(Pos.CENTER);
 
 		ops = new ComboBox<String>();
 		ops.getItems().addAll("+", "-", "*", "/");
@@ -60,27 +62,32 @@ public class CalculadoraCompleja extends Application {
 
 		HBox complejo1 = new HBox();
 		complejo1.setAlignment(Pos.BASELINE_CENTER);
-		complejo1.getChildren().addAll(real1, suma, imagin1, i);
+		complejo1.setSpacing(5);
+		complejo1.getChildren().addAll(real1, new Label("+"), imagin1, new Label("i"));
 
 		HBox complejo2 = new HBox();
-		complejo2.setAlignment(Pos.BOTTOM_CENTER);
-		complejo2.getChildren().addAll(real2, suma, imagin2, i);
+		complejo2.setAlignment(Pos.BASELINE_CENTER);
+		complejo2.setSpacing(5);
+		complejo2.getChildren().addAll(real2, new Label("+"), imagin2, new Label("i"));
 
 		HBox resultadoComplejo = new HBox();
 		resultadoComplejo.setAlignment(Pos.BASELINE_CENTER);
-		resultadoComplejo.getChildren().addAll(resul1, suma, resul2);
+		resultadoComplejo.setSpacing(5);
+		resultadoComplejo.getChildren().addAll(resul1, new Label("+"), resul2, new Label("i"));
 
 		VBox vbox1 = new VBox();
-		vbox1.setAlignment(Pos.BASELINE_CENTER);
-		vbox1.getChildren().addAll(complejo1, complejo2);
+		vbox1.setAlignment(Pos.CENTER);
+		vbox1.setSpacing(5);
+		vbox1.getChildren().addAll(complejo1, complejo2, separator, resultadoComplejo);
 
 		VBox comboVbox = new VBox();
-		comboVbox.setAlignment(Pos.BASELINE_LEFT);
+		comboVbox.setAlignment(Pos.CENTER);
+		comboVbox.getChildren().addAll(ops);
 
 		HBox root = new HBox();
 		root.setSpacing(5);
 		root.setAlignment(Pos.CENTER);
-		root.getChildren().addAll(complejo2);
+		root.getChildren().addAll(comboVbox, vbox1);
 
 		Scene scene = new Scene(root, 320, 200);
 		primaryStage.setScene(scene);
