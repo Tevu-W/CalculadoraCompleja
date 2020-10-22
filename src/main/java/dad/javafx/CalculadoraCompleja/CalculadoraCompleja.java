bbe5945269d9b5e94f101cbdfd4aa762a516ca88
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-10-22 14:06:16 +0000

    Update CalculadoraCompleja.java

--- src/main/java/dad/javafx/CalculadoraCompleja/CalculadoraCompleja.java
+++ src/main/java/dad/javafx/CalculadoraCompleja/CalculadoraCompleja.java
@@ -20,17 +20,17 @@ import javafx.util.converter.NumberStringConverter;
 
 public class CalculadoraCompleja extends Application {
 
-	//View
+	// View
 	private TextField real1, imagin1, real2, imagin2, resulReal, resulImagin;
 	private ComboBox<String> ops;
 	private Separator separator;
-	
-	//Model
+
+	// Model
 	private Complejo NumeroComplejo1 = new Complejo();
 	private Complejo NumeroComplejo2 = new Complejo();
 	private Complejo NumeroComplejo3 = new Complejo();
 	private StringProperty comboelegido = new SimpleStringProperty();
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
@@ -106,38 +106,38 @@ public class CalculadoraCompleja extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Calculadora Compleja");
 		primaryStage.show();
-		
-		
-		//Bindings
+
+		// Bindings
 		Bindings.bindBidirectional(real1.textProperty(), NumeroComplejo1.realProperty(), new NumberStringConverter());
-		Bindings.bindBidirectional(imagin1.textProperty(), NumeroComplejo1.imaginarioProperty(), new NumberStringConverter());
+		Bindings.bindBidirectional(imagin1.textProperty(), NumeroComplejo1.imaginarioProperty(),
+				new NumberStringConverter());
 
 		Bindings.bindBidirectional(real2.textProperty(), NumeroComplejo2.realProperty(), new NumberStringConverter());
-		Bindings.bindBidirectional(imagin2.textProperty(), NumeroComplejo2.imaginarioProperty(), new NumberStringConverter());
-				
+		Bindings.bindBidirectional(imagin2.textProperty(), NumeroComplejo2.imaginarioProperty(),
+				new NumberStringConverter());
+
 		comboelegido.bind(ops.getSelectionModel().selectedItemProperty());
-		
+
 		ops.getSelectionModel().selectedItemProperty().addListener((o, ov, nv) -> {
-			if(ops.getSelectionModel().getSelectedItem() == "+") {
+			if (ops.getSelectionModel().getSelectedItem() == "+") {
 				NumeroComplejo3 = NumeroComplejo1.add(NumeroComplejo2);
-				resulReal.textProperty().bind(NumeroComplejo3.realProperty().asString("%.0f"));
-				resulImagin.textProperty().bind(NumeroComplejo3.imaginarioProperty().asString("%.0f"));
-				//Bindings.bindBidirectional(resulReal.textProperty(), NumeroComplejo3.realProperty(), new NumberStringConverter());
-				//Bindings.bindBidirectional(resulImagin.textProperty(), NumeroComplejo3.imaginarioProperty(), new NumberStringConverter());
+				resulReal.textProperty().bind(NumeroComplejo3.realProperty().asString("%.2f"));
+				resulImagin.textProperty().bind(NumeroComplejo3.imaginarioProperty().asString("%.2f"));
 			}
-			if(ops.getSelectionModel().getSelectedItem() == "-") {
+			if (ops.getSelectionModel().getSelectedItem() == "-") {
 				NumeroComplejo3 = NumeroComplejo1.substract(NumeroComplejo2);
-				resulReal.textProperty().bind(NumeroComplejo3.realProperty().asString("%.0f"));
-				resulImagin.textProperty().bind(NumeroComplejo3.imaginarioProperty().asString("%.0f"));
-				//Bindings.bindBidirectional(resulReal.textProperty(), NumeroComplejo3.realProperty(), new NumberStringConverter());
-				//Bindings.bindBidirectional(resulImagin.textProperty(), NumeroComplejo3.imaginarioProperty(), new NumberStringConverter());
+				resulReal.textProperty().bind(NumeroComplejo3.realProperty().asString("%.2f"));
+				resulImagin.textProperty().bind(NumeroComplejo3.imaginarioProperty().asString("%.2f"));
 			}
-			if(ops.getSelectionModel().getSelectedItem() == "*") {
+			if (ops.getSelectionModel().getSelectedItem() == "*") {
 				NumeroComplejo3 = NumeroComplejo1.multiply(NumeroComplejo2);
-				resulReal.textProperty().bind(NumeroComplejo3.realProperty().asString("%.0f"));
-				resulImagin.textProperty().bind(NumeroComplejo3.imaginarioProperty().asString("%.0f"));
-				//Bindings.bindBidirectional(resulReal.textProperty(), NumeroComplejo3.realProperty(), new NumberStringConverter());
-				//Bindings.bindBidirectional(resulImagin.textProperty(), NumeroComplejo3.imaginarioProperty(), new NumberStringConverter());
+				resulReal.textProperty().bind(NumeroComplejo3.realProperty().asString("%.2f"));
+				resulImagin.textProperty().bind(NumeroComplejo3.imaginarioProperty().asString("%.2f"));
+			}
+			if(ops.getSelectionModel().getSelectedItem() == "/") {
+				NumeroComplejo3 = NumeroComplejo1.divide(NumeroComplejo2);
+				resulReal.textProperty().bind(NumeroComplejo3.realProperty().asString("%.2f"));
+				resulImagin.textProperty().bind(NumeroComplejo3.imaginarioProperty().asString("%.2f"));
 			}
 		});
 	}
