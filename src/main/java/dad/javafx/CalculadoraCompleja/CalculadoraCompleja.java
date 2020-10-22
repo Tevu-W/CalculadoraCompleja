15e1c01779470c18ca114a8abb40467915fabc53
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-10-22 14:19:08 +0000

    Update CalculadoraCompleja.java

--- src/main/java/dad/javafx/CalculadoraCompleja/CalculadoraCompleja.java
+++ src/main/java/dad/javafx/CalculadoraCompleja/CalculadoraCompleja.java
@@ -116,10 +116,12 @@ public class CalculadoraCompleja extends Application {
 		Bindings.bindBidirectional(imagin2.textProperty(), NumeroComplejo2.imaginarioProperty(),
 				new NumberStringConverter());
 
-		// Suma por Defecto
+		//Default operation
 		NumeroComplejo3 = NumeroComplejo1.add(NumeroComplejo2);
+		resulReal.textProperty().bind(NumeroComplejo3.realProperty().asString("%.2f"));
+		resulImagin.textProperty().bind(NumeroComplejo3.imaginarioProperty().asString("%.2f"));
 
-		// Operaciones a elegir
+		//Operations to choose
 		ops.getSelectionModel().selectedItemProperty().addListener((o, ov, nv) -> {
 			if (ops.getSelectionModel().getSelectedItem() == "+") {
 				NumeroComplejo3 = NumeroComplejo1.add(NumeroComplejo2);
